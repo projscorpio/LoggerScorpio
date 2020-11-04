90c495c9ebb1d18cb402fe1095b2005a9deea1ad
Miłosz Dyka <milosz.dyka@gmail.com>
2020-11-04 14:43:13 +0100

    improved logging to files, added perror support

--- src/SPDLogWrapper.cpp
+++ src/SPDLogWrapper.cpp
@@ -1,50 +1,113 @@
+#include <filesystem>
+#include <time.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
 #include <spdlog/sinks/rotating_file_sink.h>
 #include "SPDLogWrapper.hpp"
 
 namespace ScorpioLog {
 
-std::shared_ptr<spdlog::logger> SPDLogWrapper::s_ConsoleLogger;
-std::shared_ptr<spdlog::logger> SPDLogWrapper::s_FileLogger;
+std::shared_ptr<spdlog::logger> SPDLogWrapper::consoleLogger_;
+std::shared_ptr<spdlog::logger> SPDLogWrapper::fileLogger_;
 
-void SPDLogWrapper::InitConsole()
+void SPDLogWrapper::initConsole()
 {
-	s_ConsoleLogger = spdlog::stdout_color_mt("ConsoleLogger");
-	SetPatternConsole();
+	consoleLogger_ = spdlog::stdout_color_mt("ConsoleLogger");
+	setPatternConsole();
 	// all messages by default
-	s_ConsoleLogger->set_level(spdlog::level::trace);
+	consoleLogger_->set_level(spdlog::level::trace);
 }
 
-void SPDLogWrapper::InitFile(const std::string& directionary, size_t maxSize, size_t maxFiles)
+void SPDLogWrapper::initFile()
 {
-	s_FileLogger = spdlog::rotating_logger_mt("FileLogger", directionary, maxSize, maxFiles);
-	SetPatternFile();
+	backupOldFiles();
+
+	// create time string and replace each space with underscore
+	time_t currentTime = time(NULL);
+	char* dir = strdup(ctime(&currentTime));
+	for(size_t i = 0; i < strlen(dir); i++)
+		if(dir[i] == ' ') dir[i] = '_';
+
+	fileLogger_ = spdlog::rotating_logger_mt(
+		"FileLogger", 
+		fmt::format("{}{}{}", LOG_FOLDER, dir, LOG_SUFIX), 
+		DEFAULT_MAX_FILE_SIZE, 
+		DEFAULT_MAX_FILES
+		);
+
+	setPatternFile();
 	// all messages by default
-	s_FileLogger->set_level(spdlog::level::trace);
+	fileLogger_->set_level(spdlog::level::trace);
+	free(dir);
 }
 
-void SPDLogWrapper::SetLevelConsole(const std::string& level)
+void SPDLogWrapper::setLevelConsole(const std::string& level)
 {
-	if(s_ConsoleLogger)
-		s_ConsoleLogger->set_level(spdlog::level::from_str(level));
+	if(consoleLogger_)
+		consoleLogger_->set_level(spdlog::level::from_str(level));
 }
 
-void SPDLogWrapper::SetLevelFile(const std::string& level)
+void SPDLogWrapper::setLevelConsole(spdlog::level::level_enum level)
 {
-	if (s_FileLogger)
-		s_FileLogger->set_level(spdlog::level::from_str(level));
+	if(consoleLogger_)
+		consoleLogger_->set_level(level);
 }
 
-void SPDLogWrapper::SetPatternConsole(const std::string& pattern)
+void SPDLogWrapper::setLevelFile(const std::string& level)
 {
-	if(s_ConsoleLogger)
-		s_ConsoleLogger->set_pattern(pattern);
+	if (fileLogger_)
+		fileLogger_->set_level(spdlog::level::from_str(level));
 }
 
-void SPDLogWrapper::SetPatternFile(const std::string& pattern)
+void SPDLogWrapper::setLevelFile(spdlog::level::level_enum level)
 {
-	if(s_FileLogger)
-		s_FileLogger->set_pattern(pattern);
+	if (fileLogger_)
+		fileLogger_->set_level(level);
+}
+
+void SPDLogWrapper::setPatternConsole(const std::string& pattern)
+{
+	if(consoleLogger_)
+		consoleLogger_->set_pattern(pattern);
+}
+
+void SPDLogWrapper::setPatternFile(const std::string& pattern)
+{
+	if(fileLogger_)
+		fileLogger_->set_pattern(pattern);
+}
+
+void SPDLogWrapper::backupOldFiles()
+{
+	namespace fs = std::filesystem;
+	// create folder for backup
+	if (!createFolder(BACKUP_FOLER))
+	{
+		if (consoleLogger_)
+			consoleLogger_->critical("Unable to create folder {}", BACKUP_FOLER);
+		exit(0);
+	}
+	// create folder for new logs
+	if (!createFolder(LOG_FOLDER))
+	{
+		if (consoleLogger_)
+			consoleLogger_->critical("Unable to create folder {}", LOG_FOLDER);
+		exit(0);
+	}
+	else
+	{
+		fs::copy(LOG_FOLDER, BACKUP_FOLER);
+		fs::remove_all(LOG_FOLDER);
+	}
+}
+
+bool SPDLogWrapper::createFolder(const char *dir)
+{
+	namespace fs = std::filesystem;
+
+	if(!fs::exists(dir))
+		return fs::create_directory(dir);
+
+	return true;
 }
 
 } // ScorpioLog 
--- src/SPDLogWrapper.hpp
+++ src/SPDLogWrapper.hpp
@@ -1,97 +1,118 @@
 #ifndef LOG_HPP_
 #define LOG_HPP_
 
+#include <errno.h>
+#include <string.h>
+
+#ifdef _WIN32
+#define strerror_r(buf, bufsz, errnum) strerror_s(buf, bufsz, errnum)
+#endif // _WIN32
+
 #include <spdlog/spdlog.h>
 
 namespace ScorpioLog {
 
 class SPDLogWrapper final {
 public:
-	static constexpr const char* DEFAULT_FORMAT = "%^[%T] [%t] %l: %v%$";
-	static constexpr const char* DEFAULT_FILE_PATH = "logs/logfile.log";
-	static constexpr size_t DEFAULT_MAX_FILE_SIZE = 1048576U * 5U; // 5MB
-	static constexpr size_t DEFAULT_MAX_FILES = 10U;
-
 	SPDLogWrapper() = delete;
 	SPDLogWrapper(const SPDLogWrapper& other) = delete;
 
 	// must be called at the beggining of the programm
-	static void InitConsole();
-	static void InitFile(const std::string& directionary = DEFAULT_FILE_PATH, size_t maxSize = DEFAULT_MAX_FILE_SIZE, size_t maxFiles = DEFAULT_MAX_FILE_SIZE);
+	static void initConsole();
+	static void initFile();
 
-	template <typename... Params>
-	static void trace(Params &&... params)
+	template <typename String, typename... Params>
+	static void trace(const String &string, const Params &... params)
 	{
-		if (s_ConsoleLogger)
-			s_ConsoleLogger->trace(params...);
-		if (s_FileLogger)
-			s_FileLogger->trace(params...);
+		if (consoleLogger_)
+			consoleLogger_->trace(string, params...);
+		if (fileLogger_)
+			fileLogger_->trace(string, params...);
 	}
 
-
-	template <typename... Params>
-	static void info(Params &&... params)
+	template <typename String, typename... Params>
+	static void info(const String &string, const Params &... params)
 	{
-		if (s_ConsoleLogger)
-			s_ConsoleLogger->info(params...);
-		if (s_FileLogger)
-			s_FileLogger->info(params...);
+		if (consoleLogger_)
+			consoleLogger_->info(string,params...);
+		if (fileLogger_)
+			fileLogger_->info(string, params...);
 	}
 
-
-	template <typename... Params>
-	static void warn(Params &&... params)
+	template <typename String, typename... Params>
+	static void warn(const String &string, const Params &... params)
 	{
-		if (s_ConsoleLogger)
-			s_ConsoleLogger->warn(params...);
-		if (s_FileLogger)
-			s_FileLogger->warn(params...);
+		if (consoleLogger_)
+			consoleLogger_->warn(string, params...);
+		if (fileLogger_)
+			fileLogger_->warn(string, params...);
 	}
 
-
-	template <typename... Params>
-	static void error(Params &&... params)
+	template <typename String, typename... Params>
+	static void error(const String &string, const Params &... params)
 	{
-		if (s_ConsoleLogger)
-			s_ConsoleLogger->error(params...);
-		if (s_FileLogger)
-			s_FileLogger->error(params...);
+		if (consoleLogger_)
+			consoleLogger_->error(string, params...);
+		if (fileLogger_)
+			fileLogger_->error(string, params...);
 	}
 
-
-	template <typename... Params>
-	static void critical(Params &&... params)
+	template <typename String, typename... Params>
+	static void perror(const String &string, const Params &... params)
 	{
-		if (s_ConsoleLogger)
-			s_ConsoleLogger->critical(params...);
-		if (s_FileLogger)
-			s_FileLogger->critical(params...);
+		char message[PERR_BUF_LEN];
+		const char *error_msg = strerror_r(errno, message, PERR_BUF_LEN);
+		error(string, error_msg, params...);
 	}
 
+	template <typename String, typename... Params>
+	static void critical(const String &string, const Params &... params)
+	{
+		if (consoleLogger_)
+			consoleLogger_->critical(string, params...);
+		if (fileLogger_)
+			fileLogger_->critical(string, params...);
+	}
 
-	static void SetLevelConsole(const std::string& level);
-	static void SetLevelFile(const std::string& level);
+	static void setLevelConsole(const std::string& level);
+	static void setLevelConsole(spdlog::level::level_enum level);
+	static void setLevelFile(const std::string& level);
+	static void setLevelFile(spdlog::level::level_enum level);
 
-	static void SetPatternConsole(const std::string& pattern = DEFAULT_FORMAT);
-	static void SetPatternFile(const std::string& pattern = DEFAULT_FORMAT);
+	static void setPatternConsole(const std::string& pattern = DEFAULT_FORMAT);
+	static void setPatternFile(const std::string& pattern = DEFAULT_FORMAT);
 
 private:
-	static std::shared_ptr<spdlog::logger> s_ConsoleLogger;
-	static std::shared_ptr<spdlog::logger> s_FileLogger;
+
+	static void backupOldFiles();
+	static bool createFolder(const char* dir);
+
+	static constexpr const char* DEFAULT_FORMAT = "%^[%T] [%t] %l: %v%$";
+	static constexpr const char* LOG_FOLDER = "logs/";
+	static constexpr const char* BACKUP_FOLER = "logs_backup/";
+	static constexpr const char* LOG_SUFIX = ".log";
+	static constexpr size_t DEFAULT_MAX_FILE_SIZE = 1048576U * 5U; // 5MB
+	static constexpr size_t DEFAULT_MAX_FILES = 20U;
+
+	static constexpr size_t PERR_BUF_LEN = 256;
+	static std::shared_ptr<spdlog::logger> consoleLogger_;
+	static std::shared_ptr<spdlog::logger> fileLogger_;
 };
 
 } // ScorpioLog 
 
 #ifdef SCORPIO_LOG
 
-#define LOGGER_INIT_CONSOLE  ScorpioLog::SPDLogWrapper::InitConsole()
-#define LOGGER_INIT_FILE     ScorpioLog::SPDLogWrapper::InitFile()
+#define LOGGER_INIT_CONSOLE  ScorpioLog::SPDLogWrapper::initConsole()
+#define LOGGER_INIT_FILE     ScorpioLog::SPDLogWrapper::initFile()
 				    
 #define LOGGER_TRACE(...)    ScorpioLog::SPDLogWrapper::trace(__VA_ARGS__)
 #define LOGGER_INFO(...)     ScorpioLog::SPDLogWrapper::info(__VA_ARGS__)
 #define LOGGER_WARN(...)     ScorpioLog::SPDLogWrapper::warn(__VA_ARGS__)
 #define LOGGER_ERROR(...)    ScorpioLog::SPDLogWrapper::error(__VA_ARGS__)
+#define LOGGER_PERROR(...)    ScorpioLog::SPDLogWrapper::perror(__VA_ARGS__)
 #define LOGGER_CRITICAL(...) ScorpioLog::SPDLogWrapper::critical(__VA_ARGS__)
+
 
 #else // SCORPIO_LOG
 
@@ -102,6 +123,7 @@ private:
 #define LOGGER_INFO(...)     
 #define LOGGER_WARN(...)     
 #define LOGGER_ERROR(...)    
+#define LOGGER_PERROR(...)
 #define LOGGER_CRITICAL(...) 
 
 #endif // SCORPIO_LOG
