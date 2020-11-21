cdc4a74f765a383014b4b6e3e781a8c690819b1a
Miłosz Dyka <milosz.dyka@gmail.com>
2020-11-21 22:29:45 +0100

    check if ci passes

--- scripts/buildall.sh
+++ scripts/buildall.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-export CC=/usr/bin/gcc-9
-export CXX=/usr/bin/g++-9
 [ ! -d "./build/" ] && mkdir build
 cd build/ && cmake .. && sudo make install
 sudo ldconfig
--- scripts/ci_env_setup.sh
+++ scripts/ci_env_setup.sh
@@ -3,3 +3,5 @@
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 sudo apt update
 sudo apt install gcc-9 g++-9
+export CC=/usr/bin/gcc-9
+export CXX=/usr/bin/g++-9
